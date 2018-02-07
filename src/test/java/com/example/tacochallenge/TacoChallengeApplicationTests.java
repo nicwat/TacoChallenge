b8bc55267d32e85853254ead04b058e38e219eb2
Nicole Watkins <nicole.watkins@detroitlabs.com>
2018-02-07 11:32:22 -0500

    Adding TacoChallengeApplicationTests.java
    Adding a folder to contain tests

---
+++ src/test/java/com/example/tacochallenge/TacoChallengeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.tacochallenge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TacoChallengeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
