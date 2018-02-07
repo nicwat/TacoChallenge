f42f9735ea7e503b58c7b96820afdec4631dd772
Nicole Watkins <nicole.watkins@detroitlabs.com>
2018-02-07 15:19:34 -0500

    Reconfiguring Customer.java
    Configuring customer_id and phone number composite primary key

--- src/main/java/com/example/tacochallenge/Customer.java
+++ src/main/java/com/example/tacochallenge/Customer.java
@@ -1,8 +1,18 @@
 package com.example.tacochallenge;
 
-import org.hibernate.mapping.Set;
+import java.util.Set;
 
+import org.hibernate.mapping.Set;
 import javax.persistence.*;
+
+
+import javax.persistence.CascadeType;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 @Entity // Tells Hibernate to make a table out of this class
 public class Customer {
@@ -15,25 +25,23 @@ public class Customer {
 //    private Integer id;
     private String name;
     private String email;
-    private String phone;
+//    private String phone;
 
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
     private Set<OrderDetail> orderDetails;
 
     public Customer(){}
 
-    public Customer(CustomerId customerId, String name, String email, String phone){
+    public Customer(CustomerId customerId, String name, String email,){
         this.customerId = customerId;
         this.name = name;
         this.email = email;
-        this.phone = phone;
     }
 
     public Customer(CustomerId customerId, String name, String email, String phone, Set<OrderDetail> orderDetails){
         this.customerId = customerId;
         this.name = name;
         this.email = email;
-        this.phone = phone;
         this.orderDetails= orderDetails;
     }
 
@@ -68,13 +76,13 @@ public class Customer {
         return this.email;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
+//    public void setPhone(String phone) {
+//        this.phone = phone;
+//    }
+//
+//    public String getPhone() {
+//        return phone;
+//    }
 
     public void setOrderDetails(Set<OrderDetail> orderDetails){
         this.orderDetails = orderDetails;
@@ -82,6 +90,28 @@ public class Customer {
 
     public Set<OrderDetail> getOrderDetails(){
         return this.orderDetails;
+    }
+
+    public String toString(){
+        String info = "";
+        JSONObject jsonInfo = new JSONObject();
+        jsonInfo.put("customerId",this.customerId.getCustomerId());
+        jsonInfo.put("phone", this.customerId.getPhone());
+        jsonInfo.put("name", this.getName());
+        jsonInfo.put("email", this.getEmail());
+
+        JSONArray orders = new JSONArray();
+        if(this.orderDetails != null){
+            this.orderDetails.forEach(order->{
+                JSONObject orderInfo = new JSONObject();
+                orderInfo.put("orderId-" + order.getOrderId() , order);
+                orders.put(orderInfo);
+            });
+        }
+        jsonInfo.put("history", orders);
+        info = jsonInfo.toString();
+        return info;
+
     }
 
 
