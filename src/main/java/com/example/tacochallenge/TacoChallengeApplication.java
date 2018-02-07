907dc448729c3eba3ebefae003108e7f79932788
Nicole Watkins <nicole.watkins@detroitlabs.com>
2018-02-07 11:30:20 -0500

    Committing TacoChallengeApplication.java
    Creates standalone application.

---
+++ src/main/java/com/example/tacochallenge/TacoChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacochallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoChallengeApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoChallengeApplication.class, args);
+	}
+}
